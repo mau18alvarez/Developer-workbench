c273a9a9dca0e09ea93860432c4df2dda49d28c9
Unknown <mau18alvarez@gmail.com>
2017-06-02 23:00:22 -0600

    Creacion del login

---
+++ src/LogIn/LogInController.java
@@ -0,0 +1,15 @@
+package LogIn;
+
+import javafx.fxml.FXML;
+
+import javax.xml.soap.Text;
+import java.awt.*;
+
+/**
+ * Created by mauri on 6/2/2017.
+ */
+public class LogInController {
+
+    @FXML private TextField login_ip;
+    @FXML private TextField login_port;
+}
