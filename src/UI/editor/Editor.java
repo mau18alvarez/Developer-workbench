6961835ea73d154ed94412611fc080ec0b2a0bbd
campos-97 <joseacampos97@gmail.com>
2017-06-07 23:04:46 -0600

    Create Table UI
    Se crea la interfaz que permite crear nuevas tablas en la DB

--- src/UI/editor/Editor.java
+++ src/UI/editor/Editor.java
@@ -1,14 +1,24 @@
 package UI.editor;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.TreeCell;
 import javafx.scene.control.TreeItem;
 import javafx.scene.control.TreeView;
+import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Rectangle;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import main.Main;
 import sun.reflect.generics.tree.Tree;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -36,6 +46,7 @@ public class Editor implements Initializable {
         TreeItem<String> insert = new TreeItem<>("Insert", new Rectangle(4,4));
         TreeItem<String> delete = new TreeItem<>("Delete", new Rectangle(4,4));
         TreeItem<String> update = new TreeItem<>("Update", new Rectangle(4,4));
+
         TreeItem<String> table = new TreeItem<>("Table", icon);
         TreeItem<String> index = new TreeItem<>("Index", icon2);
         root.getChildren().addAll(create,drop,select,insert,delete,update);
@@ -44,6 +55,20 @@ public class Editor implements Initializable {
 
         leftmenu.setRoot(root);
         leftmenu.setShowRoot(false);
+        leftmenu.getSelectionModel().selectedItemProperty()
+                .addListener((v,oldValue,newValue) -> {
+                    switch (newValue.getValue()){
+                        case "Table": {
+                            try {
+                                Main.createTableStage();
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                            break;
+                        }
+                    }
+                });
 
     }
+
 }
